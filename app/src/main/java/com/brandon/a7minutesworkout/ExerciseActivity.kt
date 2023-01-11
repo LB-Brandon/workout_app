2ea9fda2326063fc3af140e8f283ef98d2581535
LB-Brandon <rmsdud623@gmail.com>
2023-01-12 01:29:27 +0900

    Feat: 툴바 생성

--- app/src/main/java/com/brandon/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/brandon/a7minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,20 @@ package com.brandon.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brandon.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var binding : ActivityExerciseBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
+
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
     }
 }
