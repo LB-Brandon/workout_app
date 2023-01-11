910e87da5a198856ffe4b5cb2261214f7b018110
LB-Brandon <rmsdud623@gmail.com>
2023-01-11 18:40:22 +0900

    Style UI settings

---
+++ app/src/main/java/com/brandon/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.brandon.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener{
+            Toast.makeText(
+                this@MainActivity,
+                "HERE WE ARE",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
