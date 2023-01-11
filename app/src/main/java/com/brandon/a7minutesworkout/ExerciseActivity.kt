7b3f1fd40ec7e2817a2ff30cbf9e2eb12da39150
LB-Brandon <rmsdud623@gmail.com>
2023-01-12 01:29:27 +0900

    Feat: 툴바 생성

--- app/src/main/java/com/brandon/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/brandon/a7minutesworkout/ExerciseActivity.kt
@@ -2,6 +2,7 @@ package com.brandon.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.brandon.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
@@ -10,12 +11,21 @@ class ExerciseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
 
         binding = ActivityExerciseBinding.inflate(layoutInflater)
+
         setContentView(binding?.root)
 
         setSupportActionBar(binding?.toolbarExercise)
+
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            Log.e("back", "왜 뒤로 안가짐")
+            onBackPressed()
+        }
 
     }
 }
