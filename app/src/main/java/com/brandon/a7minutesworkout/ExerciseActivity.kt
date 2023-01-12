86b7e6c398a102c62d39a5c12da5672839ea5d31
LB-Brandon <rmsdud623@gmail.com>
2023-01-12 14:44:04 +0900

    Feat: Move to Exercise UI
    1. Change visibility

--- app/src/main/java/com/brandon/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/brandon/a7minutesworkout/ExerciseActivity.kt
@@ -2,20 +2,27 @@ package com.brandon.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.brandon.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding : ActivityExerciseBinding? = null
 
+    private var restTimer : CountDownTimer? = null
+    private var restProgress = 0
+
+    private var exerciseTimer : CountDownTimer? = null
+    private var exerciseProgress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityExerciseBinding.inflate(layoutInflater)
-
         setContentView(binding?.root)
-
         setSupportActionBar(binding?.toolbarExercise)
 
         if(supportActionBar != null) {
@@ -23,9 +30,81 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         binding?.toolbarExercise?.setNavigationOnClickListener {
-            Log.e("back", "왜 뒤로 안가짐")
             onBackPressed()
         }
+//        binding?.flProgressBar?.visibility = View.GONE    완전히 사라지게 함
+//        binding?.flProgressBar?.visibility = View.INVISIBLE   보이지만 않으며 자리를 차지함
 
+        setUpRestView()
+    }
+
+    private fun setupExerciseView(){
+//        binding?.flProgressBar?.visibility = View.GONE
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "Exercise"
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        setExerciseProgressBar()
+    }
+
+    private fun setUpRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000){
+
+            override fun onTick(millisUntilFinished: Long) {
+                Log.e("song", "rest")
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                setupExerciseView()
+                Toast.makeText(this@ExerciseActivity, "Here now we will start the exercise", Toast.LENGTH_LONG).show()
+            }
+
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        binding?.progressBarExercise?.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000, 1000){
+
+            override fun onTick(millisUntilFinished: Long) {
+                Log.e("song", "exercise")
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "30 Seconds are over, lets go to the rest view", Toast.LENGTH_LONG).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        binding = null
     }
 }
